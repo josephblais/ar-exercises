916b0a2599421eb714b7f9e3a7101cba7e334f65
Joseph Blais <josephpierreblais@gmail.com>
2021-03-29 20:27:11 -0400

    Complete exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store 
+  has_many :employees 
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Billy Bob", last_name: "Thorton", hourly_rate: 20)
+@store1.employees.create(first_name: "John", last_name: "Cena", hourly_rate: 75)
+@store2.employees.create(first_name: "Whitney", last_name: "Houston", hourly_rate: 100)
+@store2.employees.create(first_name: "Abby", last_name: "Winter", hourly_rate: 12)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# class Employee < ActiveRecord::Base
+#   belongs_to :store
+#   validates :first_name, :last_name, :store_id, :presence => true
+#   validates :hourly_rate, numericality: {greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+# end
+
+# class Store < ActiveRecord::Base
+#   has_many: employees
+#   validates :name, length {minimum 3}
+#   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+# end
+
+gets.chomp = Store.create(name: "#{gets.chomp}")
